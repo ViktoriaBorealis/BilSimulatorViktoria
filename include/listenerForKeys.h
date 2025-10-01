becba5ed51c89cd8d8036b4a03e672edbfc109bb
Vikky <viktoria@solbu.org>
2025-10-01 19:43:35 +0200

    lil test changes

--- include/listenerForKeys.h
+++ include/listenerForKeys.h
@@ -6,10 +6,15 @@ struct MyListener : threepp::KeyListener {
 
     MyListener(carClass& ourCar): ourCar(ourCar)
          {}
-    //why doesnt this get uploaded to github?
 
     void onKeyPressed(threepp::KeyEvent evt) override {
         if (evt.key == threepp::Key::SPACE) {
+            ourCar.hop();
+        }
+    }
+    void onKeyRepeat(threepp::KeyEvent evt) override { //Instead of this we can use a toggle.
+        //because this is slow
+        if (evt.key == threepp::Key::W) {
             ourCar.hop();
         }
     }
